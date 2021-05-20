efcec196430fb0fff20ea87d07312a417484c485
Muhammad Fajri Rasid <fajrimuhammad460@gmail.com>
2021-05-20 19:57:55 +0800

    changed colors.xml and themes.xml

--- app/src/main/java/com/example/proyek/SplashScreenActivity.java
+++ app/src/main/java/com/example/proyek/SplashScreenActivity.java
@@ -2,7 +2,12 @@ package com.example.proyek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        new Handler().postDelayed(() -> {
+            Intent mainActivity = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(mainActivity);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        }, 2000);
     }
 }
