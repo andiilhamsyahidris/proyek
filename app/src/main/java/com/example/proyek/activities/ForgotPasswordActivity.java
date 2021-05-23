bac85346fb8546fd6762ca6e94f7f39f7fdbc660
Muhammad Fajri Rasid <fajrimuhammad460@gmail.com>
2021-05-23 11:52:17 +0800

    Updated fragment_fav.xml fragment_inbox.xml fragment_history.xml, Added ForgotPasswordActivity and Some dialog_box layout

---
+++ app/src/main/java/com/example/proyek/activities/ForgotPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.example.proyek.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyek.R;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
