010d430555a6b7303ead2b518010fbe4f517bb58
Richard Enrico <richardenrico02@gmail.com>
2021-05-24 10:14:34 +0800

    minor changes

--- app/src/main/java/com/example/proyek/activities/GetStartedActivity.java
+++ app/src/main/java/com/example/proyek/activities/GetStartedActivity.java
@@ -3,89 +3,93 @@ package com.example.proyek.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.proyek.R;
+import com.example.proyek.auth.SignInActivity;
+import com.example.proyek.auth.SignUpActivity;
 import com.google.android.material.button.MaterialButton;
 
-public class GetStartedActivity extends AppCompatActivity implements View.OnClickListener {
-//    private Button btnSignIn;
-//    private TextView tvSignUp;
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_get_started);
-//
-//        btnSignIn = findViewById(R.id.btnSignIn);
-//        tvSignUp = findViewById(R.id.tvSignUp);
-//
-//        btnSignIn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(GetStartedActivity.this, SignInActivity.class);
-//                startActivity(intent);
-//                finish();
-//            }
-//        });
-//
-//        tvSignUp.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(GetStartedActivity.this, SignUpActivity.class);
-//                startActivity(intent);
-//                finish();
-//            }
-//        });
-//    }
-//
-//    @Override
-//    public void onBackPressed(){
-//        moveTaskToBack(true);
-//    }
-
-    // kode ini digunakan hanya sekedar untuk mendemo-kan dialog box
-    // beri comment jika tidak digunakan
-    private Dialog dialogBox;
+public class GetStartedActivity extends AppCompatActivity {
+    private Button btnSignIn;
+    private TextView tvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
 
-        dialogBox = new Dialog(GetStartedActivity.this);
+        btnSignIn = findViewById(R.id.btnSignIn);
+        tvSignUp = findViewById(R.id.tvSignUp);
 
-        dialogBox.setContentView(R.layout.dialog_box_reset_cart);
-        dialogBox.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT);
-        dialogBox.setCancelable(false);
-        dialogBox.getWindow().getAttributes().windowAnimations = R.style.DialogBoxAnimation;
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GetStartedActivity.this, SignInActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
-        MaterialButton btnConfirm = dialogBox.findViewById(R.id.btnResetCartTrue);
-        btnConfirm.setOnClickListener(this);
-
-        MaterialButton btnCancel = dialogBox.findViewById(R.id.btnResetCartFalse);
-        btnCancel.setOnClickListener(this);
-
-        MaterialButton btnSignIn = findViewById(R.id.btnSignIn);
-        btnSignIn.setOnClickListener(this);
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GetStartedActivity.this, SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     @Override
-    public void onClick(View view) {
-        int id = view.getId();
-
-        if (id == R.id.btnResetCartTrue) {
-            Toast.makeText(this, "Do something here!", Toast.LENGTH_SHORT).show();
-            dialogBox.dismiss();
-        } else if (id == R.id.btnResetCartFalse) {
-            dialogBox.dismiss();
-        } else if (id == R.id.btnSignIn) {
-            dialogBox.show();
-        }
+    public void onBackPressed(){
+        moveTaskToBack(true);
     }
+
+    // kode ini digunakan hanya sekedar untuk mendemo-kan dialog box
+    // beri comment jika tidak digunakan
+//    private Dialog dialogBox;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_get_started);
+//
+//        dialogBox = new Dialog(GetStartedActivity.this);
+//
+//        dialogBox.setContentView(R.layout.dialog_box_reset_cart);
+//        dialogBox.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,
+//                ViewGroup.LayoutParams.WRAP_CONTENT);
+//        dialogBox.setCancelable(false);
+//        dialogBox.getWindow().getAttributes().windowAnimations = R.style.DialogBoxAnimation;
+//
+//        MaterialButton btnConfirm = dialogBox.findViewById(R.id.btnResetCartTrue);
+//        btnConfirm.setOnClickListener(this);
+//
+//        MaterialButton btnCancel = dialogBox.findViewById(R.id.btnResetCartFalse);
+//        btnCancel.setOnClickListener(this);
+//
+//        MaterialButton btnSignIn = findViewById(R.id.btnSignIn);
+//        btnSignIn.setOnClickListener(this);
+//    }
+//
+//    @Override
+//    public void onClick(View view) {
+//        int id = view.getId();
+//
+//        if (id == R.id.btnResetCartTrue) {
+//            Toast.makeText(this, "Do something here!", Toast.LENGTH_SHORT).show();
+//            dialogBox.dismiss();
+//        } else if (id == R.id.btnResetCartFalse) {
+//            dialogBox.dismiss();
+//        } else if (id == R.id.btnSignIn) {
+//            dialogBox.show();
+//        }
+//    }
 }
--- app/src/main/java/com/example/proyek/fragments/FragmentProfile.java
+++ app/src/main/java/com/example/proyek/fragments/FragmentProfile.java
@@ -6,8 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -24,59 +26,64 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class FragmentProfile extends Fragment {
 
-    View view;
-    private Button btnOut;
-    private FirebaseUser user;
-    private DatabaseReference reference;
-    private String userID;
-    private FirebaseAuth mAuth;
+    private View view;
+    private Toolbar tbProfile;
+    private CircleImageView civProfileImg;
+
+//    private Button btnOut;
+//    private FirebaseUser user;
+//    private DatabaseReference reference;
+//    private String userID;
+//    private FirebaseAuth mAuth;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        user = FirebaseAuth.getInstance().getCurrentUser();
-        reference = FirebaseDatabase.getInstance().getReference("User");
-        userID = user.getUid();
-        mAuth = FirebaseAuth.getInstance();
-
-        final TextView textViewName = view.findViewById(R.id.tvName);
-        final TextView textViewEmail = view.findViewById(R.id.tvEmail);
-
-        btnOut = view.findViewById(R.id.btnOut);
-        btnOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mAuth.signOut();
-                Intent a = new Intent(getActivity(), SignInActivity.class);
-                a.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(a);
-                getActivity().finish();
-            }
-        });
-
-        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                User userProfile = snapshot.getValue(User.class);
-                if (userProfile != null){
-                    String username = userProfile.username;
-                    String email = userProfile.email;
-
-                    textViewEmail.setText(email);
-                    textViewName.setText(username);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-                Toast.makeText(getActivity(), "something wrong", Toast.LENGTH_LONG).show();
-            }
-        });
+//        user = FirebaseAuth.getInstance().getCurrentUser();
+//        reference = FirebaseDatabase.getInstance().getReference("User");
+//        userID = user.getUid();
+//        mAuth = FirebaseAuth.getInstance();
+//
+//        final TextView textViewName = view.findViewById(R.id.tvName);
+//        final TextView textViewEmail = view.findViewById(R.id.tvEmail);
+//
+//        btnOut = view.findViewById(R.id.btnOut);
+//        btnOut.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                mAuth.signOut();
+//                Intent a = new Intent(getActivity(), SignInActivity.class);
+//                a.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(a);
+//                getActivity().finish();
+//            }
+//        });
+//
+//        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                User userProfile = snapshot.getValue(User.class);
+//                if (userProfile != null){
+//                    String username = userProfile.username;
+//                    String email = userProfile.email;
+//
+//                    textViewEmail.setText(email);
+//                    textViewName.setText(username);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//                Toast.makeText(getActivity(), "something wrong", Toast.LENGTH_LONG).show();
+//            }
+//        });
 
         return view;
     }
